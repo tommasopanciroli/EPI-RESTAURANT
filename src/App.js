a2cf14562eb0b8364c5b33750bb7eaa6d1c34fac
tommaso.panciroli <tommypanci@gmail.com>
2024-12-04 09:52:00 +0100

    adds images

--- src/App.js
+++ src/App.js
@@ -7,39 +7,57 @@ import CustomNavbar from './components/CustomNavbar'
 import MainContent from './components/MainContent'
 import Reservation from './components/Reservation'
 import Admin from './components/Admin'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        {/* passo due prop: una per il sottititolo della navbar e una che setterà il valore di fluid per il suo container */}
-        <CustomNavbar subtitle="Pastasciutte sul web" isFluid={true} />
-      </header>
-      <main>
-        {/* <div class="container"> */}
-        <Container>
-          {/* <div class="row"> */}
-          <Row className="justify-content-center mt-3">
-            <Col xs={12} md={8} lg={6}>
-              <Admin />
-            </Col>
-          </Row>
-          <Row className="justify-content-center mt-3">
-            <Col xs={12} md={8} lg={6}>
-              <Reservation />
-            </Col>
-          </Row>
-          <Row className="justify-content-center mt-3">
-            {/* <div class="col"> */}
-            <Col xs={12} md={8} lg={6}>
-              {/* oggi il form lo metteremo temporaneamente qua! */}
-              {/* qui metto il carosello */}
-              <MainContent />
-            </Col>
-          </Row>
-        </Container>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          {/* passo due prop: una per il sottititolo della navbar e una che setterà il valore di fluid per il suo container */}
+          <CustomNavbar subtitle="Pastasciutte sul web" isFluid={true} />
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              {/* creo una rotta per l'home page */}
+              <Route
+                path="/"
+                element={
+                  <Row className="justify-content-center mt-3">
+                    <Col xs={12} md={8} lg={6}>
+                      <MainContent />
+                    </Col>
+                  </Row>
+                }
+              />
+              {/* creo una rotta per la pagian di amministrazione */}
+              <Route
+                path="/amministrazione"
+                element={
+                  <Row className="justify-content-center mt-3">
+                    <Col xs={12} md={8} lg={6}>
+                      <Admin />
+                    </Col>
+                  </Row>
+                }
+              />
+
+              <Route
+                path="/prenota"
+                element={
+                  <Row className="justify-content-center mt-3">
+                    <Col xs={12} md={8} lg={6}>
+                      <Reservation />
+                    </Col>
+                  </Row>
+                }
+              />
+            </Routes>
+          </Container>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
